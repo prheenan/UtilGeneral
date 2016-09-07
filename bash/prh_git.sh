5382b2de08e75c9646aed3d366576755eebf9902
patrick heenan <patrick.heenan@colorado.edu>
2016-09-07 10:29:51 -0600

    improved git setup, added configration of names

--- bash/prh_git.sh
+++ bash/prh_git.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-MyDir="$HOME/src_prh"
-git clone https://github.com/prheenan/Research $MyDir/Research
-git clone https://github.com/prheenan/BioModel $MyDir/FitUtil
-git clone https://github.com/prheenan/IgorUtil $MyDir/IgorUtil
+dir="$HOME/src_prh"
+ResDir="$dir/Research"
+FitDir="$dir/FitUtil"
+IgorDir="$dir/IgorUtil"
 
+git clone --progress -v https://github.com/prheenan/Research "$ResDir"
+git clone --progress -v https://github.com/prheenan/BioModel "$FitDir"
+git clone --progress -v https://github.com/prheenan/IgorUtil "$IgorDir"
+
+# configure ids; defaults to just us
+function Ids
+{
+	current=`pwd`
+	cd "$1"
+	git config --local user.email "patrick.heenan@colorado.edu"
+	git config --local user.name "patrick heenan"
+	cd "$current"
+} 
+Ids "$ResDir"
+Ids "$FitDir"
+Ids "$IgorDir"
