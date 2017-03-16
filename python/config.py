174384c1e0b6a51b8f8df32405cfda3bf01737a1
Your Name <patirck.heenan@colorado.edu>
2017-03-16 01:15:06 -0600

    added in better imports to python config

--- python/config.py
+++ python/config.py
@@ -1,5 +1,8 @@
 # force floating point division. Can still use integer with //
 from __future__ import division
+# other good compatibility recquirements for python3
+from __future__ import absolute_import
+from __future__ import print_function
 # This file is used for importing the common utilities classes.
 import numpy as np
 import matplotlib.pyplot as plt
