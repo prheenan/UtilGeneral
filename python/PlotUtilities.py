ec5957cb9cbca54e181ff4fdbbe6d810ca575ec2
patrick heenan <patrick.heenan@colorado.edu>
2017-09-15 15:34:57 -0600

    when removing tick labels, the padding now goes down to -1...

--- python/PlotUtilities.py
+++ python/PlotUtilities.py
@@ -125,6 +125,9 @@ def FormatImageAxis(ax=None):
 
 def _remove_labels(ax):
     ax.set_ticklabels([])
+    # if we remove the tick labels, then we bring the label (e.g. y axis name)
+    # down 
+    ax.labelpad=-1
 
 def _remove_ticks(ax):
     ax.set_ticks([])
