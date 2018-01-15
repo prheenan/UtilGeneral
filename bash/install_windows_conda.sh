5f75efdb800984958f7a1ee4a09c6475f2cfbf93
Your Name <patirck.heenan@colorado.edu>
2018-01-15 10:38:43 -0700

    threw out everything except python

--- bash/install_windows_conda.sh
+++
@@ -1,9 +0,0 @@
-function python_install {
-	conda install $@ --yes
-	conda update $@
-}
-python_install numpy
-python_install scipy
-python_install matplotlib
-python_install pip
-python -m pip install igor
