fe999696a558d42099df817efd66309a4d078bc1
Patrick B24-MD <patrick.heenan@colorado.edu>
2018-07-20 16:35:58 -0600

    changed how padding worked slightly. added save pdf option.

--- PlotUtilities.py
+++ PlotUtilities.py
@@ -781,7 +781,7 @@ def pm(stdOrMinMax,mean=None,fmt=".3g"):
     return ("{:"+ fmt + "}+/-{:.2g}").format(mean,delta)
 
 def savefig(figure,fileName,close=True,tight=True,subplots_adjust=None,
-            bbox_inches='tight',pad=1,pad_inches=0.02,**kwargs):
+            bbox_inches='tight',pad=0,pad_inches=0.02,**kwargs):
     """
     Saves the given figure with the options and filenames
     
@@ -796,7 +796,7 @@ def savefig(figure,fileName,close=True,tight=True,subplots_adjust=None,
         nothing
     """
     if (tight):
-        plt.tight_layout(pad=pad)
+        figure.tight_layout(pad=pad)
     if (subplots_adjust is not None):
         plt.subplots_adjust(**subplots_adjust)
     baseName = util.getFileFromPath(fileName)
@@ -915,7 +915,7 @@ def save_png_and_svg(fig,base,**kwargs):
     """
     save_twice(fig,base +".png",base+".svg",**kwargs)
     
-def save_tom(fig,base,save_tiff=False,**kwargs):
+def save_tom(fig,base,save_tiff=False,save_pdf=False,**kwargs):
     """
     Saves however tom would like me to 
     
@@ -923,6 +923,8 @@ def save_tom(fig,base,save_tiff=False,**kwargs):
     """
     if save_tiff:
         savefig(fig,base + ".tiff",close=False,**kwargs)
+    if save_pdf:
+        savefig(fig,base + ".pdf",close=False,**kwargs)
     save_twice(fig,base +".jpeg",base+".svg",**kwargs)
     
 # legacy API. plan is now to mimic matplotlib 
