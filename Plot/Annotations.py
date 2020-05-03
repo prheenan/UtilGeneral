d1c9cb4281093cd9e59ecda0fbce91c700ef8536
Patrick Laptop (MBP) <patirck.heenan@colorado.edu>
2020-05-03 11:40:33 -0400

    annottate now using text argument for mpl 3 compatibility

--- Plot/Annotations.py
+++ Plot/Annotations.py
@@ -62,7 +62,7 @@ def _annotate(ax,s,xy,**font_kwargs):
             font_kwargs[k] = v
     # POST: all default added
     dict_sanit = sanitize_text_dict(font_kwargs)
-    return ax.annotate(s, xy=xy,**dict_sanit)
+    return ax.annotate(text=s, xy=xy,**dict_sanit)
     
 def relative_annotate(ax,s,xy,xycoords='axes fraction',**font_kwargs):
     """
